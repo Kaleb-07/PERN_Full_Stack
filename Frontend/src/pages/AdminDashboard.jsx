10945528e054cc4903172c6d6408ea411bb043f9
Kaleb Mulugeta Estifanos <kalebmulugeta07@gmail.com>
2026-08-08 14:59:34 -0500

    Update AdminDashboard.jsx

--- Frontend/src/pages/AdminDashboard.jsx
+++ Frontend/src/pages/AdminDashboard.jsx
@@ -90,7 +90,7 @@ const AdminDashboard = () => {
         </button>
       </nav>
 
-      {/* --- DYNAMIC CONTENT */}
+      {/* DYNAMIC CONTENT */}
       <main className="admin-content">
         <AnimatePresence mode="wait">
           <motion.div 
