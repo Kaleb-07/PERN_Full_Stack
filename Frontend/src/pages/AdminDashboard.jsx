699971b964483f7f728b0cae3151f08d69421f76
Kaleb Mulugeta Estifanos <kalebmulugeta07@gmail.com>
2026-09-14 12:56:46 -0500

    Update AdminDashboard.jsx

--- Frontend/src/pages/AdminDashboard.jsx
+++ Frontend/src/pages/AdminDashboard.jsx
@@ -38,7 +38,7 @@ const AdminDashboard = () => {
 
   return (
     <div className="admin-dashboard-container">
-      {/* ELITE HEADERS */}
+      {/* The ELITE HEADERS */}
       <header className="admin-header">
         <div className="admin-header-title">
           <motion.h1
